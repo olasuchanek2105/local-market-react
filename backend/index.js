9dddf95a7121103e596d947a006a6066b534c64e
Ola Suchanek <olasuchanek21@gmail.com>
2026-06-12 19:22:44 +0200

    connect React frontend to Express backend

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,12 @@ const listings = [
 
 
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World")
