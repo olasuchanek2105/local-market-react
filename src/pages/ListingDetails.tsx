7918664d463e565cf86e5c02e9f98445214e7f8f
Ola Suchanek <olasuchanek21@gmail.com>
2026-06-12 11:17:26 +0200

    add listing details

--- src/pages/ListingDetails.tsx
+++ src/pages/ListingDetails.tsx
@@ -1,10 +1,25 @@
+import { useParams } from "react-router";
+import { listings } from "../data/listings";
 
 
 export function ListingDetails(){
+    const params = useParams();
+
+    const listing = listings.find(listing => listing.id === Number(params.id))
+
+    if (!listing){
+        return;
+    } 
+
     return(
         <div>
-            Szczegoly ogloszenia
+            <h1>Szczegoly ogloszenia</h1>
+            <h2>{listing.title}</h2>
+            <p>Miasto: {listing.city}</p>
+            <p>Cena: {listing.price} złotych</p>
+            <p>Kategoria: {listing.category}</p>
         </div>
     )
 }
 
+export default ListingDetails
