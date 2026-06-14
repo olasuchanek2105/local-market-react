c81a0fb957c902403dc4fd67b45ddf9099dba25c
Ola Suchanek <olasuchanek21@gmail.com>
2026-06-14 22:16:57 +0200

    add Navbar to App and fix route order

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import { Route, Routes } from "react-router"
 import ListOfProducts from "./pages/ListOfProducts"
 import { ListingDetails } from "./pages/ListingDetails"
 import AddListing from "./pages/AddListing"
+import Navbar from "./components/Navbar"
 
 function App() {
-  return(
-    // <div><ListOfProducts/> 
-    // </div>
-  <Routes>
-    <Route path="/" element={<ListOfProducts />} />
-    <Route path="/listings" element={<ListOfProducts />} />
-    <Route path="/listings/:id" element={<ListingDetails />} />
-    <Route path="/listings/add" element={< AddListing />} />
-  </Routes>
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ListOfProducts />} />
+        <Route path="/listings" element={<ListOfProducts />} />
+        <Route path="/listings/add" element={<AddListing />} />
+        <Route path="/listings/:id" element={<ListingDetails />} />
+      </Routes>
+    </div>
   )
-
 }
 
 export default App
